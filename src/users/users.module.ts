ce611e79b366b868ec8e030bc669315b5300af60
ashir <ashir@metrictreelabs.com>
2024-02-13 11:09:33 +0530

    added schema and db connection

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { usersSchema } from './schemas/users.schema';
 
 @Module({
+  imports:[MongooseModule.forFeature([{name:'users',schema: usersSchema}])],
   controllers: [UsersController],
   providers: [UsersService]
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { users } from './schemas/users.schema';
+import mongoose from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel(users.name)
+        private userModel: mongoose.Model<users>
+    ){}
+
+    async findAll():Promise<users[]>{
+        const users = await this.userModel.find({})
+        return users
+    }
+
+}
